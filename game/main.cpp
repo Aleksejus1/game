0599036feb8eedecef72f935c7ba7ce42a5a1968
Aleksejus1 <justis1971212@gmail.com>
2015-11-23 17:21:15 +0200

    day 1 - update

--- game/main.cpp
+++ game/main.cpp
@@ -3,10 +3,11 @@
 #include <vector>
 #include <string>
 #include <algorithm>
+#include <sstream>
 
 using namespace std;
 
-/* CONSTANTS */				
+/* CONSTANTS */					
 #define ARRAY_SIZE(array) (sizeof((array))/sizeof((array[0])))
 #define STRING_UPPER(string) (transform(string.begin(), string.end(), string.begin(), ::toupper))
 string
@@ -16,34 +17,51 @@ vector<string>
 	answer_yes (answer_yes_s, answer_yes_s+ ARRAY_SIZE(answer_yes_s)),
 	answer_no(answer_no_s, answer_no_s + ARRAY_SIZE(answer_no_s));
 const int LINE_LENGTH = 80;
+const int currencyExchangeValue = 12; //12:1 a.k.a. 12 of lesser value = 1 of greater value
 
-/* Struct initiation */		
+/* Struct initiation */			
 struct option_;
 struct page_;
+struct meter_;
+struct entity_;
+struct money_;
+struct player_;
+struct area_;
 struct menu_;
-/* Functions initiation */	
+/* Functions initiation */		
 bool defaultCondition();
-bool stringInArray(string, vector<string>);
+bool stringInArray(string _string, vector<string> _vector);
+string base12(unsigned long long _number);
 bool isNumberInBetween(int number, int betweenThis, int andThis);
-bool isNumber(const std::string&);
-void writeInMiddle(string);
+bool isNumber(const std::string& _string);
+bool areThereAnySavedGames();
+void writeInMiddle(string _text);
 void clear();
+void init();
 void pageInit();
 void pause();
 
-/* Enum definitions */		
+/* Enum definitions */			
 enum pageTypes {
 	PAGE_TYPE_DEFAULT,
 	PAGE_TYPE_YESNO
 };
-/* Struct definitions */	
+/* Struct definitions */		 	
 struct option_ {
 	bool(*condition)() = defaultCondition;
-	option_(string _name) {
+	option_(string _name, bool(*_condition)() = defaultCondition) {
 		name = _name;
+		condition = _condition;
 	}
 	string name;
 	page_* page;
+};
+struct money_{
+	int sum = 0;
+	string currencyIndicator;
+	string getString() {
+		return base12(sum);
+	}
 };
 struct page_ {
 
@@ -73,8 +91,10 @@ struct page_ {
 		else if (stringInArray(answer, answer_no)) return 0;
 		else return -1;
 	}
-	void addOption(string _name) {
-		options.push_back(option_(_name));
+	page_* addOption(string _name, bool(*_condition)() = nullptr) {
+		if(_condition == nullptr) options.push_back(option_(_name));
+		else options.push_back(option_(_name, _condition));
+		return this;
 	}
 	void writeIntro() {
 		writeInMiddle(intro);
@@ -107,27 +127,81 @@ struct page_ {
 		return answeredOptionID;
 	}
 };
+struct meter_ {
+	float
+		current,
+		max;
+	string outOf() {
+		stringstream ss;
+		ss << fixed << setprecision(2) << current << "/" << max;
+		return ss.str();
+	}
+};
+struct entity_ {
+	meter_ health;
+};
+struct area_ {
+	string name = "default";
+	page_ page;
+	area_(string _name) {
+		if (_name != "") name = _name;
+	}
+};
 struct menu_ {
 	page_ main;
 	menu_() {
 		main.intro = "Welcome to a game";
-		main.addOption("New Game");
-		main.addOption("Continue");
-		main.addOption("Load Game");
-		main.addOption("Options");
-		main.addOption("Exit");
+		main
+			.addOption("New Game")
+			->addOption("Continue", areThereAnySavedGames)
+			->addOption("Load Game", areThereAnySavedGames)
+			->addOption("Options")
+			->addOption("Exit");
+	}
+};
+struct areas_ : vector<area_>{
+	area_& add(string _name) {
+		push_back((area_)(_name));
+		return back();
+	}
+	void init() {
+		add("Main");
+		add("Forest");
+		add("Village").page
+			.addOption("Enter a Bar")
+			->addOption("Enter the Sherifs' office")
+			->addOption("Enter the mayor's office");
+		add("Swamp");
+		add("Cave");
 	}
 };
 
-/* Global Variables */
+/* Global Variables */			
+areas_ areas;
 page_ exitGame;
 menu_ menu;
 
-/* Functions definitions */	
+/* Struct definitions part2 */	
+struct player_ : entity_ {
+	area_ *area = nullptr;
+	money_ money;
+	void init() {
+		area = &areas[2];
+		money.sum+=59;
+	} // areas must be pre-initiated before calling this
+};
+
+/* Global Variables part2 */	
+player_ player;
+
+/* Functions definitions */ 			
 int main() {
-	pageInit();
-	while (menu.main.viableOptions[menu.main.show()].name != "Exit");
-	cout << exitGame.show();
+	init();
+	/*game*/
+	cout 
+		<< "Health[" << player.health.outOf() << "]" 
+		<< " - Area[" << player.area->name << "]"
+		<< " - Souls[" << player.money.getString() << "]";
 	pause();
 	return 0;
 }
@@ -138,9 +212,22 @@ bool stringInArray(string _string, vector<string> _vector) {
 	}
 	return false;
 }
+void init() {
+	pageInit();
+	areas.init();
+	player.init();
+}
+string base12(unsigned long long _number) {
+	if (_number < 12)
+		return string() + "0123456789AB"[_number];
+	return base12(_number / 12) + base12(_number % 12);
+}
 void pageInit() {
 	exitGame.intro = "Are you sure you want to exit the game?";
 	exitGame.type = PAGE_TYPE_YESNO;
+}
+bool areThereAnySavedGames() {
+	return false;
 }
 bool defaultCondition() {
 	return true;
@@ -159,8 +246,8 @@ void pause() {
 	cout << endl << "------------------------------------------------" << endl;
 	system("pause");
 }
-bool isNumber(const std::string& s){
-	return !s.empty() && find_if(s.begin(), s.end(), [](char c) { return !isdigit(c); }) == s.end();
+bool isNumber(const std::string& _string){
+	return !_string.empty() && find_if(_string.begin(), _string.end(), [](char c) { return !isdigit(c); }) == _string.end();
 }
 bool isNumberInBetween(int number, int betweenThis, int andThis) {
 	return (number >= betweenThis && number <= andThis);
